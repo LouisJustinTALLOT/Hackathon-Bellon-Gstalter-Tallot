ef7294e349b7cfc5fd7cc71e58659e327e4d2551
Judith <judith.bellon@mines-paristech.fr>
2021-01-29 10:29:29 +0100

    Une classe heros

---
+++ heros.py
@@ -0,0 +1,39 @@
+import pygame as pg
+
+class Heros:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+        self.width = 30 # largeur du heros en pixels
+        self.height = 30 # hauteur du heros en pixels
+
+    def affiche_heros(self, screen):
+        rect = pg.Rect(self.x, self.y, self.width, self.height)
+        color = (0, 0, 255) 
+        pg.draw.rect(screen, color, rect)
+    
+    def deplacement(self, direction):
+        self.x += direction[0]
+        self.y += direction[1]
+
+
+
+
+if __name__ == "__main__":
+    pg.init()
+    screen = pg.display.set_mode((400, 300))
+    perso = Heros(20,20)
+    clock = pg.time.Clock()
+    running = True
+    while running:
+        clock.tick(1)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_q:
+                    running = False
+        perso.affiche_heros(screen)
+        perso.deplacement((0,20))
+        pg.display.update()
+
