5c12d2933d630b9c3df6c0ba8863e67cd77a0cb0
plgstalter <pierrelouisgstalter67@gmail.com>
2021-01-29 11:34:34 +0100

    affichage d'un bonnhomme

--- display.py
+++ display.py
@@ -1,16 +1,16 @@
 import pygame as pg, sys
 from random import randint
+import src.get_levels as gl
+import numpy as np
 
 def init(n, m):
-    screen = init_display(n, m)
-    add_borders(screen, n, m)
+    screen = init_display(16*n, 16*m)
     return screen
 
 def init_display(n, m):
     screen = pg.display.set_mode((n, m))
     screen.fill((89, 90, 92))
     return screen
-
 
 def add_borders(screen, n, m):
     width = 5 # largeur du rectangle en pixels
@@ -26,3 +26,35 @@ def add_borders(screen, n, m):
         pg.draw.rect(screen, color, rect)
         rect = pg.Rect(i, m-5, width, height)
         pg.draw.rect(screen, color, rect)
+
+
+
+def affichage(screen, matrice):
+    images = [pg.image.load("images/sol.png"), pg.image.load("images/heros.png"), pg.image.load("images/mur.png")]
+    n, m = len(matrice), len(matrice[0])
+    for i in range(n):
+        for j in range(m):
+            screen.blit(images[matrice[i][j]], (16*i, 16*j))
+            
+list_levels = gl.load_all_levels()
+mat = list_levels[0].matrice_niveau
+
+mat = np.array(mat)
+mat = mat.T
+n, m = len(mat), len(mat[0])
+
+screen = init(n, m)
+affichage(screen, mat)
+
+clock = pg.time.Clock()
+running = True
+while running:
+    clock.tick(1)
+    for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_q:
+                    running = False       
+    pg.display.update()
+pg.quit()
